b41066d1184aee36be288b6f1bcddf630931a9e7
Christinec98 <40578200+Christinec98@users.noreply.github.com>
2023-12-08 21:32:49 -0800

    Styled by christine

--- app.py
+++ app.py
@@ -6,21 +6,48 @@ import json
 
 app = Flask(__name__)
 # Enable CORS for all routes with explicit origin
-CORS(app, resources={r"/test/*": {"origins": "*", "methods": ["POST", "OPTIONS"], "headers": ["Content-Type", "Authorization"]}})
+CORS(app, resources={r"/test/*": {"origins": "http://127.0.0.1:5500", "methods": ["POST", "OPTIONS"], "allow_headers": "Content-Type,Authorization", "allow_credentials": True}})
+# CORS(app)  # Enable CORS for all routes
 
-@app.route('/test/search', methods=['POST'])
+
+@app.route('/test/search', methods=['POST', 'OPTIONS'])
 def handle_request():
-    data = request.get_json()
+    if request.method == 'OPTIONS':
+        # Respond to the preflight request
+        # response = jsonify()
+        response = jsonify()
+        response.headers.add('Access-Control-Allow-Origin', '*')  # Change * to your specific front-end domain in production
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'POST')
+        return response
+       
+    else:
+        data = request.get_json()
 
-    query = data.get('query')
-    k = data.get('k', 5)  # Default to 5 if 'k' is not provided
-    print(data)
-    scrape_output = do_scrape(query, False)
-    search_output = get_knn(query, scrape_output, k)
+        query = data.get('query')
+        k = data.get('k', 5)  # Default to 5 if 'k' is not provided
+        print(data)
+        scrape_output = do_scrape(query, False)
+        search_output = get_knn(query, scrape_output, k)
+        response = jsonify(search_output)
 
-    json_response = json.dumps(search_output)
-    print(json_response)
-    return jsonify(json_response)
+        json_response = json.dumps(search_output)
+        print(json_response)
+        # response = jsonify(json_response)
+        response.headers.add('Content-Type', 'application/json')
+
+
+    # response.headers['Access-Control-Allow-Origin'] = 'http://127.0.0.1:5500'
+    # response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
+    # # response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Accept'
+    # response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    # # response.headers['Access-Control-Allow-Credentials'] = 'true'
+    return response
 
 if __name__ == '__main__':
     app.run(port=5000)
+
+
+
+
+
